30b10208ed0611c8438abd7e6d6b4d787e7f6d1c
Gunali Rezqi Mauludi <hello.gunalirezqi@gmail.com>
2019-07-29 07:49:12 +0700

    update db

--- database/absensi_new.sql
+++ database/absensi_new.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 16/06/2019 22:25:19
+ Date: 29/07/2019 07:48:34
 */
 
 SET NAMES utf8mb4;
@@ -39,7 +39,9 @@ CREATE TABLE `infoabsensi` (
 -- Records of infoabsensi
 -- ----------------------------
 BEGIN;
-INSERT INTO `infoabsensi` VALUES ('2019-06-14', 'P0001', '08:00:00', NULL, 'd7:66:37:d8:92', '1.928738792', '9.7637783983', '-', NULL, '1');
+INSERT INTO `infoabsensi` VALUES ('2019-07-18', 'P0001', '06:35:33', '16:48:30', '00:00:00:00:00:00', '-6.90779940225184', '107.74408643133938', '-', '-', '1');
+INSERT INTO `infoabsensi` VALUES ('2019-07-19', 'P0001', '07:48:19', '13:37:09', '00:00:00:00:00:00', '-6.90779940225184', '107.74408643133938', '-', 'Pulang Cepat', '1');
+INSERT INTO `infoabsensi` VALUES ('2019-07-23', 'P0002', '08:15:42', '17:15:46', NULL, NULL, NULL, NULL, NULL, '1');
 COMMIT;
 
 -- ----------------------------
@@ -50,6 +52,8 @@ CREATE TABLE `mpegawai` (
   `nik` varchar(25) NOT NULL,
   `nama` varchar(150) DEFAULT NULL,
   `alamat` text,
+  `tanggallahir` date DEFAULT NULL,
+  `jabatan` enum('Sopir','Helper','Satpam','Ka.Gudang','Finance','Adm','Adm Sales','RSM','ASM','HRD') DEFAULT NULL,
   `password` varchar(32) DEFAULT NULL,
   `macaddress` varchar(25) DEFAULT NULL,
   `status` char(1) DEFAULT '1',
@@ -60,8 +64,9 @@ CREATE TABLE `mpegawai` (
 -- Records of mpegawai
 -- ----------------------------
 BEGIN;
-INSERT INTO `mpegawai` VALUES ('P0001', 'Budi', 'Jl. Samudra', 'e10adc3949ba59abbe56e057f20f883e', '28:28:s8:92:2o', '1');
-INSERT INTO `mpegawai` VALUES ('P0002', 'Rangga', 'Jl. Cikutra', 'e10adc3949ba59abbe56e057f20f883e', '9f:7d:9h:24:56', '1');
+INSERT INTO `mpegawai` VALUES ('P0001', 'Budi', 'Jl. Samudra', '2019-06-16', 'Adm', 'e10adc3949ba59abbe56e057f20f883e', '28:28:s8:92:2o', '1');
+INSERT INTO `mpegawai` VALUES ('P0002', 'Rangga', 'Jl. Cikutra', '2019-06-03', 'HRD', 'e10adc3949ba59abbe56e057f20f883e', '9f:7d:9h:24:56', '1');
+INSERT INTO `mpegawai` VALUES ('P0003', 'Hilman', 'Jl. Sadang Serang', '2019-07-29', 'Satpam', 'e10adc3949ba59abbe56e057f20f883e', '3z:1o:1h:82:11', '1');
 COMMIT;
 
 -- ----------------------------
@@ -81,29 +86,7 @@ CREATE TABLE `mperusahaan` (
 -- Records of mperusahaan
 -- ----------------------------
 BEGIN;
-INSERT INTO `mperusahaan` VALUES (0, 'PT DINUS CIPTA MANDIRI', 'Jl. Holis, Caringin, Bandung Kulon, Kota Bandung, Jawa Barat, Indonesia', '-6.932276371608801', '107.57246031481941');
-COMMIT;
-
--- ----------------------------
--- Table structure for muser
--- ----------------------------
-DROP TABLE IF EXISTS `muser`;
-CREATE TABLE `muser` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `foto` varchar(50) DEFAULT NULL,
-  `nama` varchar(125) DEFAULT NULL,
-  `username` varchar(30) DEFAULT NULL,
-  `password` varchar(32) DEFAULT NULL,
-  `status` char(1) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
--- ----------------------------
--- Records of muser
--- ----------------------------
-BEGIN;
-INSERT INTO `muser` VALUES (1, NULL, 'Gunali Rezqi Mauludi', 'gunalirezqi', 'e10adc3949ba59abbe56e057f20f883e', '1');
-INSERT INTO `muser` VALUES (2, NULL, 'Abdu R Ruchendar', 'abdu', 'e10adc3949ba59abbe56e057f20f883e', '1');
+INSERT INTO `mperusahaan` VALUES (0, 'PT DINUS CIPTA MANDIRI', 'Jl. Holis, Caringin, Bandung Kulon, Kota Bandung, Jawa Barat, Indonesia', '-6.907805353403091', '107.74406279437244');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
